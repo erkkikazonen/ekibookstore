37af1bec66127f7a7795c1c156febf7a36dba270
Erkki <erkki.kazonen@gmail.com>
2023-09-13 18:40:05 +0300

    second commit

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,14 +1,25 @@
 package com.example.Bookstore.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
+
+
 @Controller
 public class BookController {
 
-	@GetMapping("/index")
-	public String index(Model model) {
+	@Autowired
+	private BookRepository repository;
 
+	@GetMapping("/booklist")
+	public String booklist(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
 	}
 }
