7362cadbb91d9fc259cd409f3a1ff8a4d686a4a8
Erkki <erkki.kazonen@gmail.com>
2023-09-13 17:56:27 +0300

    first commit

--- src/main/java/com/example/Bookstore/domain/Book.java
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -1,13 +1,37 @@
 package com.example.Bookstore.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int isbn;
 	private String title;
 	private String author;
 	private int publicationYear;
-	private int isbn;
 	private double price;
 	
+	public Book(int isbn, String title, String author, int publicationYear) {
+		super();
+		this.isbn = isbn;
+		this.title = title;
+		this.author = author;
+		this.publicationYear = publicationYear;
+	}
+	public long getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
 	public String getTitle() {
 		return title;
 	}
@@ -26,16 +50,22 @@ public class Book {
 	public void setPublicationYear(int publicationYear) {
 		this.publicationYear = publicationYear;
 	}
-	public int getIsbn() {
-		return isbn;
-	}
-	public void setIsbn(int isbn) {
-		this.isbn = isbn;
-	}
 	public double getPrice() {
 		return price;
 	}
 	public void setPrice(double price) {
 		this.price = price;
+	}
+	
+	public class BookCommandLineRunner implements CommandLineRunner {
+
+	    @Autowired
+	    private BookRepository bookRepository;
+
+	    @Override
+	    public void run(String... args) throws Exception {
+	        bookRepository.save(new Book( 0001, "Paasikivi", "Alpo Juntunen", 2021));
+	        bookRepository.save(new Book(0002, "Samuli", "Johannes Lahtela", 2021));
+	    }
 	}
 }
