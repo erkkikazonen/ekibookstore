e0bcb3123e3f4d2bbec595b4392686ba8da1f9e2
Erkki <erkki.kazonen@gmail.com>
2023-09-13 18:53:32 +0300

    third commit

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
@@ -22,4 +26,14 @@ public class BookController {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+	@PostMapping("/addbook")
+	 public String addbook(Book book) {
+	  repository.save(book);
+	  return "redirect:/booklist";
+	 }
+	@DeleteMapping("/deletebook/{isbn}")
+	 public String deletebook(@PathVariable int isbn) {
+	  repository.deleteById(isbn);
+	  return "redirect:/booklist";
+	 }
 }
